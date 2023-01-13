ff063d01e5da028b99403deb391edf164d486b08
mosidrum <mosiokanga@gmail.com>
2023-01-14 00:43:51 +0100

    transfered project from my Redux repo

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
+import SearchBar from './searchBar';
+import ImageList from './ImageList';
 
-function App () {
-  return (
-    <div className="Container">
-      <h2>Photo Lab</h2>
-    </div>
-  );
-}
+
+class App extends React.Component {
+
+    state = {images : [] };
+
+   onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos',{
+            params : {query : term},
+        });
+        this.setState({images : response.data.results});
+    }
+
+    render () {
+        return (
+            <div className='ui container' style = {{marginTop: '10px'}}>
+                    <SearchBar onSubmit = {this.onSearchSubmit} />
+                    <ImageList images={this.state.images} />
+             </div>
+        );
+    }
+};
 
 export default App;
